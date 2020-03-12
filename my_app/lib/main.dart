dff018fcc07d362befbebc822f2a35c1dbe880ed
jahgath <jagathjijo01@gmail.com>
2020-03-12 19:07:56 +0530

    stateless widget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[800],
-    ),
-    body: Center(
-      child: Text(
-        'hello ninjas',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey,
-          fontFamily: 'Bellota'
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[800],
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'Bellota'
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('+'),
-      onPressed: (){},
-      backgroundColor: Colors.red[800],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        child: Text('Meh'),
+        onPressed: (){},
+        backgroundColor: Colors.red[800],
+      ),
+    );
+  }
+}
+
