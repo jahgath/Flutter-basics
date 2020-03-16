6ae83cb40eb7ebfcd1336d99872b4d0be30128ed
jahgath <jagathjijo01@gmail.com>
2020-03-16 22:51:56 +0530

    stateful widgets

--- app_id/lib/main.dart
+++ app_id/lib/main.dart
@@ -16,56 +16,33 @@ class AppCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30,40,30,0),
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
               child: CircleAvatar(
-                backgroundImage: AssetImage('assets/avatar.png'),
+                backgroundColor: Colors.white,
                 radius: 40,
               ),
             ),
             Divider(
-              height: 60,
-              color: Colors.grey[800],
-              
+              height: 50,
+              color: Colors.grey,
             ),
             Text(
-              'NAME',
-               style: TextStyle(
-                 color: Colors.grey,
-                 letterSpacing: 2,
-               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Chun-Li',
+              'Name',
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 28,
-                color: Colors.amber,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
+                fontSize: 20,
                 color: Colors.grey,
                 letterSpacing: 2,
               ),
             ),
             Text(
-              '8',
+              'Chun-Li',
               style: TextStyle(
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
-                fontSize: 28,
                 color: Colors.amber,
                 letterSpacing: 2,
               ),
@@ -73,30 +50,58 @@ class AppCard extends StatelessWidget {
             SizedBox(
               height: 30,
             ),
+            Text(
+              'Name',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
             Row(
               children: <Widget>[
                 Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
+                  Icons.camera,
+                  color: Colors.grey,
                 ),
                 SizedBox(
-                  width: 10,
+                  width: 20,
                 ),
                 Text(
-
-                  'chun.li@protonmail.com',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      letterSpacing: 1,
-                      fontSize: 18,
-                    ),
+                  'Ninja',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ],
-            ),
+            )
           ],
         ),
       ),
     );
+  }
+}
 
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
